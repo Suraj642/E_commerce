36723578c09ad7a1fd2f1e4c8df85fa373809b6a
Suraj642 <surajkumarmaurya642@gmail.com>
2023-10-19 01:46:59 +0530

    db folder

--- admin/user/register_insert.php
+++ admin/user/register_insert.php
@@ -1,3 +1,9 @@
 <?php
-print_r($_POST);
+if(isset($_REQUEST['submit'])){
+   echo  $name=$_REQUEST['name'];
+    echo $email=$_REQUEST['email'];
+    echo $phone=$_REQUEST['number'];
+    echo $address=$_REQUEST['address'];
+   echo  $password=$_REQUEST['password'];
+}
 ?>
---
+++ db/db.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect('loacalhost','root','ecommerce');
+print_r($con);
+
+?>
